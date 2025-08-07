5ec0343a565a58a796e59ddb552645683930ac82
Justin Horst <justin.horst@salesforce.com>
2025-08-07 13:44:12 -0400

    remove unused templates

--- AndroidIDPTemplate/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "SalesforceAndroidIDPTemplateApp"
-
-include(":app")
-
-val salesforceMobileSdkRoot = File("mobile_sdk/SalesforceMobileSDK-Android")
-if (salesforceMobileSdkRoot.exists()) {
-    includeBuild(salesforceMobileSdkRoot)
-}
--- AndroidNativeLoginTemplate/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "AndroidNativeLoginTemplate"
-
-include(":app")
-
-val salesforceMobileSdkRoot = File("mobile_sdk/SalesforceMobileSDK-Android")
-if (salesforceMobileSdkRoot.exists()) {
-    includeBuild(salesforceMobileSdkRoot)
-}
